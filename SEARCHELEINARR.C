e96f611d8ff54069853d066528173e78c3ef84e4
lmohanraj <35128040+lmohanraj@users.noreply.github.com>
2018-02-18 11:30:09 +0530

    Create SEARCHELEINARR.C

---
+++ SEARCHELEINARR.C
@@ -0,0 +1,22 @@
+ #include<stdio.h>
+ int main()
+ {
+ 	int n,k,a[10],c=0,i;
+ 	printf("no of elements\n");
+ 	scanf("%d",&n);
+ 	printf("enter the elements\n");
+ 	for(i=0;i<n;i++)
+            scanf("%d",&a[i]);
+            printf("enter element to search\n");
+            scanf("%d",&k);
+           	for(i=0;i<n;i++)
+           	{
+           		if(a[i]==k)
+           		c++;
+           	}
+            if(c==0)
+            printf("no");
+            else
+            printf("yes");
+            return 0;
+  }
